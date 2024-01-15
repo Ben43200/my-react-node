4a8eba7a8aeb87e719504f2970740639726e3aca
meteoben <meteoben@gmail.com>
2024-01-15 13:48:37 +0100

    essai avec le fix de railway port et "0.0.0.0"

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+const port = process.env.PORT || 8000;
 app.get("/message", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.listen(8000, () => {
+app.listen(port, "0.0.0.0",() => {
   console.log(`Server is running on port 8000.`);
 });
