4a6b33a75a31151d1ada4ad85a752dfed99473be
meteoben <meteoben@gmail.com>
2024-01-22 14:22:14 +0100

    push new adress re dailway en fetch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:8000")
+    fetch("http://172.17.0.164:7423/")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
